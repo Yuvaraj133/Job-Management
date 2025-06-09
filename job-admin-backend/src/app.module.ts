c9bca84ce7f7ebdbc7e286b2a6970c07ad18a9a5
Yuvaraj133 <113129721+Yuvaraj133@users.noreply.github.com>
2025-06-09 19:04:16 +0530

    Update app.module.ts

--- job-admin-backend/src/app.module.ts
+++ job-admin-backend/src/app.module.ts
@@ -14,7 +14,7 @@ import { Job } from './jobs/job.entity';
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
         host: configService.get<string>('DB_HOST'),
-        port: parseInt(configService.get<string>('DB_PORT'), 10), // ✅ FIXED
+       port: parseInt(configService.get<string>('DB_PORT') || '5432', 10),
         username: configService.get<string>('DB_USERNAME'),
         password: configService.get<string>('DB_PASSWORD'),
         database: configService.get<string>('DB_NAME'),
